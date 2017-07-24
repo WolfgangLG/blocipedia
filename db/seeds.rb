fac7617d9293bc53831207f25decb342a9553c82
Ludwig Gerdes <lgerdes@g2crowd.com>
2017-07-24 12:00:00 -0500

    Seed user and wiki data into database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(
+    username:     "WolfgangLG",
+    first_name:   "Ludwig",
+    last_name:    "Gerdes",
+    email:        "gerdesludwig@gmail.com",
+    password:     "password",
+    role:         1,
+    confirmed_at: Date.today
+)
+
+5.times do
+  fn = Faker::Name.first_name
+  ln = Faker::Name.last_name
+  User.create!(
+      username:     Faker::Internet.user_name("#{fn} #{ln}", %w(-)),
+      first_name:   fn,
+      last_name:    ln,
+      email:        Faker::Internet.free_email,
+      password:     Faker::Lorem.characters(10),
+      role:         Random.rand(2),
+      confirmed_at: Date.today
+  )
+end
+users = User.all
+
+5.times do
+  Wiki.create!(
+      title: Faker::Lorem.sentences(2),
+      body:  Faker::Lorem.sentences(3),
+      private: Faker::Boolean.boolean,
+      user: users.sample
+  )
+end
