856544cd4d06123dd59468de87ae84bf9ffb8d5b
Ludwig Gerdes <lgerdes@g2crowd.com>
2017-07-30 12:00:00 -0500

    Convert custom users templates from embedded ruby to slim

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: {
-      registrations: 'users/registrations'
+      registrations: 'users/registrations',
+      sessions: 'users/sessions',
+      passwords: 'users/passwords'
     }
 
   resources :charges, only: [:new, :create]
